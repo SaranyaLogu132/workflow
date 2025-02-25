ddecf5b1c87fbf24d994296791eee31f44870f2f
SaranyaLogu132 <122966632+SaranyaLogu132@users.noreply.github.com>
2025-02-25 13:15:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = var.s3_bucket_name
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Unique S3 bucket name"
+  type        = string
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
+}
